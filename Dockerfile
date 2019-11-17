f6aea9b68646cb188117a75534ccbd3e86af1606
MadVodka <ivanshblu@gmail.com>
2019-11-17 19:37:03 +0400

    Logging added Available for creating image for Docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM tomcat
 COPY tomcat-users.xml /usr/local/tomcat/conf/tomcat-users.xml
+COPY context.xml /usr/local/tomcat/webapps/manager/META-INF/
+
 COPY ./target/rest_app-1.0-SNAPSHOT.war /usr/local/tomcat/webapps/rest-app.war
 CMD ["catalina.sh","run"]
--- src/main/java/ivan/vatlin/rest/TestRestController.java
+++ src/main/java/ivan/vatlin/rest/TestRestController.java
@@ -1,7 +1,7 @@
 package ivan.vatlin.rest;
 
 import org.slf4j.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,16 +10,17 @@ import java.util.Map;
 
 @RestController
 public class TestRestController {
-    @Autowired
-    private Logger logger;
+    private static final Logger logger = LoggerFactory.getLogger(TestRestController.class);
 
     @GetMapping("message")
     public Map<String, String> getMessage() {
+        logger.info("Rest controller \"message\" is invoked");
         return Collections.singletonMap("message", "Hello!");
     }
 
     @GetMapping("greetings")
     public Map<String, String[]> getGreetings() {
+        logger.info("Rest controller \"greetings\" is invoked");
         String[] greetings = {"Good morning", "Good afternoon", "Good evening", "Hi", "Hello", "What's up!"};
         return Collections.singletonMap("greetings", greetings);
     }
